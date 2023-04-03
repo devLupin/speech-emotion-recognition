b3b7e9af9e09582e25259f590f93e833eb680fc1
devLupin <luckyquit49@gmail.com>
2023-04-03 19:41:49 +0900

    cleaning

---
+++ paper implementation/Config.py
@@ -0,0 +1,19 @@
+# Emotional Tags Configuration
+class Config:
+    # Emotional Tags in EMODB
+    EMODB_LABELS = ("angry", "boredom", "disgust", "fear", "happy", "neutral", "sad")
+    EMODB_K = 10
+    # Emotional Tags in SAVEE
+    SAVEE_LABELS = ("angry","disgust", "fear", "happy", "neutral", "sad", "surprise")
+    SAVEE_K = 4
+    # Emotional Tags in RAVDESS
+    RAVDESS_LABELS = ("angry", "calm", "disgust", "fear", "happy", "neutral","sad","surprise")
+    RAVDESS_K = 5
+    # Emotional Tags in CREMA
+    CREMA_LABELS = ("sad", "angry", "disgust", "fear", "happy", "neutral")
+    CREMA_K = 5
+    # Emotional Tags in TESS
+    TESS_LABELS = ("angry", "disgust", "fear", "happy", "neutral", "pleasant_surprise", "sad")
+    TESS_K = 2
+    # Emotional Tags in IEMOCAP
+    IEMOCAP_LABELS = ("angry", "neutral", "sad", "happy")
---
+++ paper implementation/Module/Config.py
@@ -0,0 +1,14 @@
+# Emotional Tags Configuration
+class Config:
+    # Emotional Tags in CASIA
+    CASIA_LABELS = ("angry", "fear", "happy", "neutral", "sad", "surprise")
+    # Emotional Tags in EMODB
+    EMODB_LABELS = ("angry", "boredom", "disgust", "fear", "happy", "neutral", "sad")
+    # Emotional Tags in SAVEE
+    SAVEE_LABELS = ("angry","disgust", "fear", "happy", "neutral", "sad", "surprise")
+    # Emotional Tags in RAVDESS
+    RAVDESS_LABELS = ("angry", "calm", "disgust", "fear", "happy", "neutral","sad","surprise")
+    # Emotional Tags in CREMA
+    CREMA_LABELS = ("sad", "angry", "disgust", "fear", "happy", "neutral")
+    # Emotional Tags in TESS
+    TESS_LABELS = ("angry", "disgust", "fear", "happy", "neutral", "pleasant_surprise", "sad")
